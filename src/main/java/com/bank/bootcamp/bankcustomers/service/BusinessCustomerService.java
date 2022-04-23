6e3ff373d1a3a3f11f8d7ef6f40adfde42a96a5c
Brayan Jenzy La Cruz Carreño <blacruzc@emeal.nttdata.com>
2022-04-22 23:24:35 -0500

    Renombrar variable

--- src/main/java/com/bank/bootcamp/bankcustomers/service/BusinessCustomerService.java
+++ src/main/java/com/bank/bootcamp/bankcustomers/service/BusinessCustomerService.java
@@ -14,20 +14,20 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class BusinessCustomerService {
 
-  private final BusinessCustomerRepository BusinessCustomerRepository;
+  private final BusinessCustomerRepository businessCustomerRepository;
 
   public Mono<BusinessCustomer> createBusinessCustomer(BusinessCustomer customer) {
     return Mono.just(customer)
       .then(check(customer, cust -> Optional.of(cust).isEmpty(), "Customer has not data"))
       .then(check(customer, cust -> ObjectUtils.isEmpty(cust.getRuc()), "RUC is required"))
       .then(check(customer, cust -> ObjectUtils.isEmpty(cust.getBusinessName()), "Business name is required"))
-        .then(BusinessCustomerRepository.findFirstByRuc(customer.getRuc())
+        .then(businessCustomerRepository.findFirstByRuc(customer.getRuc())
             .<BusinessCustomer>handle((record, sink) -> sink.error(new BankValidationException("Customer already exists")))
-            .switchIfEmpty(BusinessCustomerRepository.save(customer)));
+            .switchIfEmpty(businessCustomerRepository.save(customer)));
   }
   
   public Mono<BusinessCustomer> findByRuc(String ruc) {
-    return BusinessCustomerRepository.findFirstByRuc(ruc);
+    return businessCustomerRepository.findFirstByRuc(ruc);
   }
 
   private <T> Mono<Void> check(T customer, Predicate<T> predicate, String messageForException) {
